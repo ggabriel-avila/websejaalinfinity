96e3183559b3aa0f545ddea33525302ee4bf4a0e
ggabriel-avila <83312216+ggabriel-avila@users.noreply.github.com>
2021-11-26 11:39:06 -0300

    Add files via upload

---
+++ scraping/scraping.py
@@ -0,0 +1,54 @@
+import pyautogui
+import time
+
+class bot:
+    '''
+        Extraccion de datos para sejaalinfinity
+    '''
+    def __init__(self):
+        pyautogui.PAUSE = 0
+        self.cambiar_seccion()
+        self.abrir_consola()
+        contador = 0
+        while(True):
+            contador = contador + 1
+            self.iniciar_bucle()
+            time.sleep(10)
+            if contador == 10:
+                time.sleep(3)
+                self.abrir_consola()
+                time.sleep(3)
+                self.abrir_consola()
+                time.sleep(3)
+                contador = 0
+
+
+    def cambiar_seccion(self):
+        pyautogui.hotkey('altleft', 'tab')
+    
+    def iniciar_bucle(self):
+        time.sleep(1)
+        script = open('./scripts.js', 'r', encoding='UTF-8').read()
+        pyautogui.typewrite(script, 0.03)
+        pyautogui.press('enter')
+        time.sleep(10)
+
+        self.limpiar_consola()
+        self.refrescar_pagina()
+
+    def abrir_consola(self):
+        time.sleep(1)
+        pyautogui.hotkey('ctrlleft', 'shift', 'i')
+        time.sleep(1)
+
+    def limpiar_consola(self):
+        pyautogui.typewrite('clear()')
+        pyautogui.press('enter')
+    
+    def refrescar_pagina(self):
+        pyautogui.press('f5')
+
+iniciar = bot()
+
+#web.sejaal@gmail.com
+#123456789159753
---
+++ scraping/scripts.js
@@ -0,0 +1,32 @@
+
+function getElementByXpath(path) {
+ return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+function init_scrap() {
+ let cantidad = getElementByXpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/div[3]/div/div[2]/nav/span[2]').textContent.split(' of ')[1];
+ let parametros = [];
+ let formData = new FormData();
+ for (let i = 2; i <= cantidad; i++) {
+  nombre = getElementByXpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/div[3]/div/div[1]/div/div/div[2]/div[' + i + ']/div[2]/div/div/div[1]')?.textContent.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
+  ;
+  promedio_spl = getElementByXpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/div[3]/div/div[1]/div/div/div[2]/div[' + i + ']/div[6]/div/div/div')?.textContent;
+  cantidad_copas = getElementByXpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/div[3]/div/div[1]/div/div/div[2]/div[' + i + ']/div[7]/div/div/div/div/div[1]/div/span').textContent.replace(',', '');
+  cantidad_spl_victoria = parseInt(getElementByXpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/div[3]/div/div[1]/div/div/div[2]/div[' + i + ']/div[7]/div/div/div/div/div[2]').textContent.split('~')[1].split(' ')[0]);
+  parametros.push({
+   'nombre': nombre,
+   'promedio_spl': promedio_spl,
+   'cantidad_copas': cantidad_copas,
+   'cantidad_spl_victoria': cantidad_spl_victoria
+  })
+ }
+ var xmlHttp = new XMLHttpRequest();
+ xmlHttp.onreadystatechange = function () {
+  if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+   console.log(xmlHttp.responseText);
+  }
+ }
+ formData.append('parametros', JSON.stringify(parametros));
+ xmlHttp.open("post", "https://sejaalinfinity.com/core/scraping/guardarDatos.php");
+ xmlHttp.send(formData);
+}
+init_scrap();
